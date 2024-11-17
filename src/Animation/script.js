1570f602cdfdfa05eef7a8fb85a67c761eaac8b2
Jericho-R-Orienza <jorienza2019@gmail.com>
2024-11-17 00:05:32 -0500

    add animation in update()

--- src/Animation/script.js
+++ src/Animation/script.js
@@ -13,33 +13,42 @@ window.addEventListener('load', function(){
             this.spriteHeight = 256;
             this.width = this.spriteWidth;
             this.height = this.spriteHeight;
-            this.scale = 2;
+            this.scale = 1.7;
             this.x = this.canvasWidth/2 - this.width * this.scale/2;
             this.y = this.canvasHeight/2 - this.height * this.scale/2;
             this.minFrame = 0;
             this.maxFrame = 355;
+            this.frame = 0;
+            this.frameX = 10;
+            this.frameY = 9;
         }
         draw(context){
             //args 1 & 2 are dealing with parsing through the sprite sheet by row-col
             context.drawImage(this.image,
-                 6 * this.spriteWidth, 
-                 8 * this.spriteHeight,
+                 this.frameX * this.spriteWidth, 
+                 this.frameY * this.spriteHeight,
                  this.spriteWidth, 
                  this.spriteHeight, 
                  this.x, this.y, 
                  this.width * this.scale, this.height * this.scale);
-            
-
+        
         }
         update(){
-            
+            // if(this.frameX < 17) this.frameX++
+            // else this.frameX = 0;
+            this.frame = this.frame < this.maxFrame ? this.frame +1 : this.minFrame;
+            this.frameX = this.frame % 18; //divide by the number of frames you have in a single row
+            this.frameY = Math.floor(this.frame / 18);
         }
     }
 
     const mandrake = new Mandrake(canvas.width, canvas.height);
     
     function animate(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         mandrake.draw(ctx);
+        mandrake.update();
+        requestAnimationFrame(animate);
     }
 
     animate();
